b497d20fc0852d2e16968ca74a242a77bfc16c6c
pheeratchai <pheeratchai.s@kkumail.com>
2026-02-10 00:04:34 +0700

    asd

---
+++ apps/lablab06-expense/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lablab06-expense',
+  webDir: 'dist'
+};
+
+export default config;
